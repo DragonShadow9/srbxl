f475474c161c7a753528a65bac27fff54a221f59
DragonShadow9 <276484@gcloud.tisd.org>
2024-01-03 22:17:15 -0600

    added other dir

---
+++ other/git/src/main.rs
@@ -0,0 +1,29 @@
+use colored::Colorize;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() <= 1 {
+        println!("{}", "No commit message provided".red());
+        return;
+    }
+
+    let commit_message = &args[1];
+
+    let command = format!("git add --all && git commit -m \"{}\" && git push && git pull", commit_message);
+    if args.len() > 2 && &args[2] == "-o" {
+        std::process::Command::new("sh")
+            .arg("-c")
+            .arg(&command)
+            .spawn()
+            .expect("Failed to execute command");
+        println!("{}", "Git commands executed successfully".green());
+    } else {
+        std::process::Command::new("sh")
+            .arg("-c")
+            .arg(&command)
+            .output()
+            .expect("Failed to execute command");
+        println!("{}", "Git commands executed successfully".green());
+    }
+}
