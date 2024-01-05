bf48fd3131c2dd6ca086d6bf72b1ebf6b544f2a5
DragonShadow9 <276484@gcloud.tisd.org>
2024-01-05 05:25:54 -0600

    added the start for help

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use colored::Colorize;
+
+
+impl Commands {
+    fn help(){
+        println!("{}","srbxl help".yellow());
+        return;
+    }
+}
+struct Commands {
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+
+    if args.len() == 1 {
+        println!("{}","use srbxl help for more information".yellow());
+    } // test 1
+
+    if args.len() == 2 {
+        if args[1] == "help" {
+            Commands::help();
+
+        }
+    }
+
 }
